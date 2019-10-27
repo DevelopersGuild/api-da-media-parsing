66d2373449313e22181b3a83ae5222bde330bd0a
Carlos Alba <soradev4@gmail.com>
2019-10-26 19:02:55 -0700

    basic server

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,12 +1,35 @@
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
 
 import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
+
 
 object Server {
   def main(args: Array[String]): Unit = {
       implicit val system: ActorSystem = ActorSystem("media-server")
       implicit val materializer: ActorMaterializer = ActorMaterializer()
       implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+
+      val route = concat(
+        path("/upload"){
+          concat(
+            get {
+              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,  "<h1>Media Parsing Server</h1>"))
+            }
+          )
+        }
+    )
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+
   }
 }
