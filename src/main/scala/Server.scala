0e8da91451242d7772a7d2950ad2adcc5c781dbc
Carlos Alba <soradev4@gmail.com>
2019-10-26 19:31:13 -0700

    fmt

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,9 +1,9 @@
 import akka.actor.ActorSystem
-import akka.stream.{ActorMaterializer, Materializer}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.directives.FileInfo
+import akka.stream.{ActorMaterializer, Materializer}
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
@@ -11,27 +11,27 @@ import scala.io.StdIn
 
 object Server {
   def main(args: Array[String]): Unit = {
-      implicit val system: ActorSystem = ActorSystem("media-server")
-      implicit val materializer: ActorMaterializer = ActorMaterializer()
-      implicit val executionContext: ExecutionContextExecutor = system.dispatcher
+    implicit val system: ActorSystem = ActorSystem("media-server")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
-      val route = concat(
-        path("upload"){
-          concat(
-            get {
-              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Media Parsing Server</h1>"))
-            },
-            post {
-              extractRequestContext{ ctx =>
-                implicit val materializer: Materializer = ctx.materializer
-                fileUpload("fileUpload") {
-                  case(fileInfo: FileInfo, fileStream) =>
-                    complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, fileInfo.fileName))
-                }
+    val route = concat(
+      path("upload") {
+        concat(
+          get {
+            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Media Parsing Server</h1>"))
+          },
+          post {
+            extractRequestContext { ctx =>
+              implicit val materializer: Materializer = ctx.materializer
+              fileUpload("fileUpload") {
+                case (fileInfo: FileInfo, fileStream) =>
+                  complete(HttpEntity(ContentTypes.`text/plain(UTF-8)`, fileInfo.fileName))
               }
             }
-          )
-        }
+          }
+        )
+      }
     )
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
