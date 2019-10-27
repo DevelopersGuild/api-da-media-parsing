5342cb1ce1d6fbc076ffa87ac46f9fdcec2ed940
Carlos Alba <soradev4@gmail.com>
2019-10-26 19:00:20 -0700

    materialzer and actor

--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,5 +1,12 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+
+import scala.concurrent.ExecutionContextExecutor
+
 object Server {
   def main(args: Array[String]): Unit = {
-
+      implicit val system: ActorSystem = ActorSystem("media-server")
+      implicit val materializer: ActorMaterializer = ActorMaterializer()
+      implicit val executionContext: ExecutionContextExecutor = system.dispatcher
   }
 }
