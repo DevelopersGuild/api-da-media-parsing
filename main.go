b0f41a299899fe930b67bab1e0e0b2ddb9517fb1
Carlos Alba <soradev4@gmail.com>
2019-10-27 17:03:05 -0700

    setup

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main(){
-	
+import (
+	"fmt"
+	"net/http"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Uploading File")
+}
+
+func setupRoutes(){
+	http.HandleFunc("/upload", uploadFile)
+    http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	fmt.Println("Hello World")
 }
