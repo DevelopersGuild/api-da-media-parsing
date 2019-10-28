aba280fae79e97207ba15eb2955377d446581c82
Carlos Alba <soradev4@gmail.com>
2019-10-27 21:03:59 -0700

    channels

--- main.go
+++ main.go
@@ -20,62 +20,65 @@ func enableCors(w *http.ResponseWriter) {
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
-	// create gcloud client
-	ctx := context.Background()
-	client, err := storage.NewClient(ctx)
-	bucketName := "api-da-test-bucket"
+	pictureURLChannel := make(chan string)
+	go func() {
+		// create gcloud client
+		ctx := context.Background()
+		client, err := storage.NewClient(ctx)
+		bucketName := "api-da-test-bucket"
 
-	if err != nil {
-		fmt.Println(err)
-	}
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	// parse file from post requests.
-	r.ParseMultipartForm(10 << 20)
-	file, handler, err := r.FormFile("fileUpload")
+		// parse file from post requests.
+		r.ParseMultipartForm(10 << 20)
+		file, handler, err := r.FormFile("fileUpload")
 
-	if err != nil {
-		fmt.Println(err)
-	}
+		if err != nil {
+			fmt.Println(err)
+		}
 
-	defer file.Close()
-	// fileBytes, err := ioutil.ReadAll(file)
-	if err != nil {
-		fmt.Println(err)
-	}
-	// name uuid generation
-	fileExtension := strings.Split(handler.Filename, ".")[1]
-	uniqueID, err := uuid.NewRandom()
-	if err != nil {
-		fmt.Println(err)
-	}
-	uniqueIDString := uniqueID.String()
-	// Writer Object Change
-	objectattrs := storage.ObjectAttrs{
-		ContentType: handler.Header.Get("Content-Type"),
-		Name:        uniqueIDString + "." + fileExtension,
-	}
-	// handler.Filename
-	wc := client.Bucket(bucketName).Object(uniqueIDString + "." + fileExtension).NewWriter(ctx)
-	wc.ObjectAttrs = objectattrs
-	if _, err = io.Copy(wc, file); err != nil {
-		fmt.Println(err)
-	}
-	if err := wc.Close(); err != nil {
-		fmt.Println(err)
-	}
+		defer file.Close()
+		// fileBytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// name uuid generation
+		fileExtension := strings.Split(handler.Filename, ".")[1]
+		uniqueID, err := uuid.NewRandom()
+		if err != nil {
+			fmt.Println(err)
+		}
+		uniqueIDString := uniqueID.String()
+		// Writer Object Change
+		objectattrs := storage.ObjectAttrs{
+			ContentType: handler.Header.Get("Content-Type"),
+			Name:        uniqueIDString + "." + fileExtension,
+		}
+		// handler.Filename
+		wc := client.Bucket(bucketName).Object(uniqueIDString + "." + fileExtension).NewWriter(ctx)
+		wc.ObjectAttrs = objectattrs
+		if _, err = io.Copy(wc, file); err != nil {
+			fmt.Println(err)
+		}
+		if err := wc.Close(); err != nil {
+			fmt.Println(err)
+		}
 
-	// JSON FOR URL
-	jsonFile, err := os.Open("./auth.json")
-	defer jsonFile.Close()
-	jsonByteValue, _ := ioutil.ReadAll(jsonFile)
-	var result map[string]interface{}
-	json.Unmarshal([]byte(jsonByteValue), &result)
+		// JSON FOR URL
+		jsonFile, err := os.Open("./auth.json")
+		defer jsonFile.Close()
+		jsonByteValue, _ := ioutil.ReadAll(jsonFile)
+		var result map[string]interface{}
+		json.Unmarshal([]byte(jsonByteValue), &result)
 
-	if err != nil {
-		fmt.Println(err)
-	}
-	pictureURL := "https://storage.googleapis.com/" + bucketName + "/" + uniqueIDString + "." + fileExtension
-	fmt.Fprintf(w, pictureURL)
+		if err != nil {
+			fmt.Println(err)
+		}
+		pictureURLChannel <- "https://storage.googleapis.com/" + bucketName + "/" + uniqueIDString + "." + fileExtension
+	}()
+	fmt.Fprintf(w, <-pictureURLChannel)
 }
 
 func setupRoutes() {
