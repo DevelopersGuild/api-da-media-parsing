4b2bbd98504c0b4d019989338bb156d84b6ac713
Carlos Alba <soradev4@gmail.com>
2019-10-27 17:11:49 -0700

    fileupload change

--- main.go
+++ main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request){
-
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("fileUpload")
+	if err != nil {
+        fmt.Println(err)
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
+    fmt.Printf("File Size: %+v\n", handler.Size)
+	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	println(fileBytes)
 }
 
 func setupRoutes(){
